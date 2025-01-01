a8377e6dd0cf6c93f0e7fbe10968f4283bde7e4c
Le Quang Thang <lethang7794@gmail.com>
2025-01-02 02:27:00 +0700

    extract getBuildInfo

--- next.config.mjs
+++ next.config.mjs
@@ -4,56 +4,10 @@ import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import remarkGfm from 'remark-gfm'
 
-import { execSync } from 'node:child_process'
-
-function parseGitCommitDate(gitCommitDate) {
-  return new Date(gitCommitDate)
-}
-
-function formatDate(date) {
-  return date.toUTCString()
-}
-
-function getBuildInfo() {
-  const gitBranch = execSync('git rev-parse --abbrev-ref HEAD', {
-    encoding: 'utf-8',
-  }).trim()
-  const gitCommitHash = execSync('git rev-parse HEAD', {
-    encoding: 'utf-8',
-  }).trim()
-  const gitCommitTag = execSync('git tag --points-at HEAD', {
-    encoding: 'utf-8',
-  }).trim()
-
-  const gitCommitDate = execSync('git show -s --format=%ci HEAD', {
-    encoding: 'utf-8',
-  }).trim()
-  const gitCommitDateParsed = parseGitCommitDate(gitCommitDate)
-  const gitCommitDateFormatted = formatDate(gitCommitDateParsed)
-
-  const buildDate = formatDate(new Date())
-
-  const env = {
-    GIT_BRANCH: (() => {
-      return gitBranch
-    })(),
-    GIT_COMMIT_HASH: (() => {
-      return gitCommitHash.slice(0, 7)
-    })(),
-    GIT_COMMIT_TAG: (() => {
-      return gitCommitTag
-    })(),
-    GIT_COMMIT_DATE: (() => {
-      return gitCommitDateFormatted
-    })(),
-    BUILD_DATE: buildDate,
-  }
-
-  return env
-}
+import { getBuildInfo } from './scripts/getBuildInfo.mjs'
 
 export default async (phase, { defaultConfig }) => {
-  const env = getBuildInfo()
+  const buildInfo = getBuildInfo()
 
   /**
    * @type {import('next').NextConfig}
@@ -61,7 +15,7 @@ export default async (phase, { defaultConfig }) => {
   const nextConfig = {
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     transpilePackages: ['next-mdx-remote'],
-    env,
+    env: { ...buildInfo },
   }
 
   const withMDX = createMDX({
---
+++ scripts/getBuildInfo.mjs
@@ -0,0 +1,45 @@
+import { execSync } from 'node:child_process'
+
+function parseGitCommitDate(gitCommitDate) {
+  return new Date(gitCommitDate)
+}
+function formatDate(date) {
+  return date.toUTCString()
+}
+export function getBuildInfo() {
+  const gitBranch = execSync('git rev-parse --abbrev-ref HEAD', {
+    encoding: 'utf-8',
+  }).trim()
+  const gitCommitHash = execSync('git rev-parse HEAD', {
+    encoding: 'utf-8',
+  }).trim()
+  const gitCommitTag = execSync('git tag --points-at HEAD', {
+    encoding: 'utf-8',
+  }).trim()
+
+  const gitCommitDate = execSync('git show -s --format=%ci HEAD', {
+    encoding: 'utf-8',
+  }).trim()
+  const gitCommitDateParsed = parseGitCommitDate(gitCommitDate)
+  const gitCommitDateFormatted = formatDate(gitCommitDateParsed)
+
+  const buildDate = formatDate(new Date())
+
+  const env = {
+    GIT_BRANCH: (() => {
+      return gitBranch
+    })(),
+    GIT_COMMIT_HASH: (() => {
+      return gitCommitHash.slice(0, 7)
+    })(),
+    GIT_COMMIT_TAG: (() => {
+      return gitCommitTag
+    })(),
+    GIT_COMMIT_DATE: (() => {
+      return gitCommitDateFormatted
+    })(),
+    BUILD_DATE: buildDate,
+  }
+
+  return env
+}
