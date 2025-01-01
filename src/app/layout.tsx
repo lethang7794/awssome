93b3e84ae059249c8ff860f0532ca9efd1b2afc4
Le Quang Thang <lethang7794@gmail.com>
2025-01-02 01:32:53 +0700

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import { cn } from '@/lib/utils'
 import './globals.css'
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   )
