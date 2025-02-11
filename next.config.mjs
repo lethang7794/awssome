329154d91d0fae53d6ac0fbdffbbcf6d91189846
Le Quang Thang <lethang7794@gmail.com>
2025-02-12 01:07:23 +0700

    let github-pages action inject next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,36 +6,31 @@ import remarkGfm from 'remark-gfm'
 
 import { getBuildInfo } from './scripts/getBuildInfo.mjs'
 
-const nextConfigFunction = async (phase, { defaultConfig }) => {
-  const buildInfo = getBuildInfo()
+const buildInfo = getBuildInfo()
 
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-    transpilePackages: ['next-mdx-remote'],
-    output: 'export',
-    env: { ...buildInfo },
-  }
-
-  const withMDX = createMDX({
-    // Add markdown plugins here, as desired
-    options: {
-      remarkPlugins: [
-        [remarkTOC, { heading: '(table[ -]of[ -])?contents?|toc|mục lục' }],
-        remarkGfm,
-      ],
-      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
-      remarkRehypeOptions: {
-        clobberPrefix: 'footnote-',
-        footnoteLabel: 'Chú thích',
-      },
-    },
-  })
-
-  // Merge MDX config with Next.js config
-  return withMDX(nextConfig)
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  transpilePackages: ['next-mdx-remote'],
+  env: { ...buildInfo },
 }
 
-export default nextConfigFunction
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [
+      [remarkTOC, { heading: '(table[ -]of[ -])?contents?|toc|mục lục' }],
+      remarkGfm,
+    ],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    remarkRehypeOptions: {
+      clobberPrefix: 'footnote-',
+      footnoteLabel: 'Chú thích',
+    },
+  },
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
